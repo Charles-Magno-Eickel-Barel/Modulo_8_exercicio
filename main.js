8215de7cbc60742865ac4d8c81738859a39bbf01
seu-usersaneme <seu-email>
2022-12-16 07:33:53 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form-atividade');
+const imgContatos = '<img src="./images/contatos.png" alt="Emoji contato" />';
+const nomes = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+
+    if (nomes.includes(inputNome.value)) {
+        alert(`O nome: ${inputNome.value} já foi inserido`);
+    } else {
+        nomes.push(inputNome.value);
+    telefones.push(parseFloat(inputTelefone.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `<td>${imgContatos} </td>`
+    linha += `</tr>`;
+
+    linhas += linha;  
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+    
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
